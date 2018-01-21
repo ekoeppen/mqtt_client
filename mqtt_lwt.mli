77b36328647bab0f2b775bcf4bfe162cb5c2b5df
Eckhart Koeppen <eck@40hz.org>
2018-01-21 20:22:26 +0200

    Don't expose connect_str

--- mqtt_lwt.mli
+++ mqtt_lwt.mli
@@ -21,13 +21,12 @@ val default_conn_opts : conn_opts_t
 val process_publish_pkt : 'a -> ('a -> string -> string -> int -> unit Lwt.t) -> unit Lwt.t
 val subscribe : ?qos:int -> topics:string list -> Lwt_io.output_channel -> unit Lwt.t
 val unsubscribe : ?qos:int -> topics:string list -> Lwt_io.output_channel -> unit Lwt.t
-val publish : ?dup:bool -> ?qos:int -> ?retain:bool -> topic:string -> 
+val publish : ?dup:bool -> ?qos:int -> ?retain:bool -> topic:string ->
   payload:string -> Lwt_io.output_channel -> unit Lwt.t
 val publish_periodically : ?qos:int -> ?period:float -> topic:string ->
   (unit -> string) -> Lwt_io.output_channel -> unit Lwt.t
 
 val connect : host:string -> port:int -> Lwt_unix.file_descr Lwt.t
 val of_socket : Lwt_unix.file_descr -> t
-val connect_str : conn_opts_t -> string
 val mqtt_client : t -> opts:conn_opts_t -> t Lwt.t
 val connect_to_broker : opts:conn_opts_t -> broker:string -> port:int -> t Lwt.t
